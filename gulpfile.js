7434caaa81baedb06e37f934e2dd15ab542f492d
Ruvim Sypa <ruvimsypa@gmail.com>
2019-02-09 10:26:31 +0200

    link

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+const gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    browserSync = require('browser-sync').create(),
+    minifyjs = require('gulp-js-minify'),
+    uglify = require('gulp-uglify'),
+    pump = require('pump'),
+    cleanCSS = require('gulp-clean-css'),
+    clean = require('gulp-clean'),
+    concat = require('gulp-concat'),
+    imagemin = require('gulp-imagemin'),
+    autoprefixer = require('gulp-autoprefixer'),
+    reload = browserSync.reload;
+sass.compiler = require('node-sass');
+
+
+const path = {
+    dist: { //Тут мы укажем куда складывать готовые после сборки файлы
+        html: 'dist/',
+        js: 'dist/js/',
+        css: 'dist/css/',
+        img: 'dist/img/',
+        fonts: 'dist/fonts/'
+    },
+    src: { //Пути откуда брать исходники
+        html: 'src/*.html', //Синтаксис src/*.html говорит gulp что мы хотим взять все файлы с расширением .html
+        js: 'src/js/main.js',//В стилях и скриптах нам понадобятся только main файлы
+        style: 'src/style/style.scss',
+        img: 'src/img/**/*.*', //Синтаксис img/**/*.* означает - взять все файлы всех расширений из папки и из вложенных каталогов
+        fonts: 'src/fonts/**/*.*'
+    },
+    watch: { //Тут мы укажем, за изменением каких файлов мы хотим наблюдать
+        html: 'src/**/*.html',
+        js: 'src/js/**/*.js',
+        style: 'src/style/**/*.scss',
+        img: 'src/img/**/*.*',
+        fonts: 'src/fonts/**/*.*'
+    },
+    clean: './dist'
+};
+const config = {
+    server: {
+        baseDir: "./dist"
+    },
+    tunnel: true,
+    host: 'localhost',
+    port: 3000,
+    logPrefix: ""
+};
+gulp.task('clean', function (){
+    return gulp.src(path.clean, {read: false})
+        .pipe(clean());
+});
+gulp.task('html', function () {
+    return gulp.src(path.src.html)
+        .pipe(gulp.dest(path.dist.html));
+});
