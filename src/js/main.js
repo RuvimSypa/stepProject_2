e015f557d6ddf5bb17a2a00978e4a59c6781e91a
Ruvim Sypa <ruvimsypa@gmail.com>
2019-02-10 23:27:23 +0200

    revo-edit

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,33 @@
 "use strict";
 
+function f(n, t) {
+  return n + t;
+}
+
+function f(n, t) {
+  return n + t;
+}
+
+function f(n, t) {
+  return n + t;
+}
+
+function f(n, t) {
+  return n + t;
+}
+
+function f(n, t) {
+  return n + t;
+}
+
+function f(n, t) {
+  return n + t;
+}
+
+function f(n, t) {
+  return n + t;
+}
+
 function f(a, b) {
   return a + b;
 }
