b0f77100fe1fb5596a458870de47b94456ccd873
Ruvim Sypa <ruvimsypa@gmail.com>
2019-02-14 14:43:46 +0200

    solution

--- src/js/main.js
+++ src/js/main.js
@@ -28,6 +28,26 @@ function f(n, t) {
   return n + t;
 }
 
+function f(n, t) {
+  return n + t;
+}
+
+function f(n, t) {
+  return n + t;
+}
+
+function f(n, t) {
+  return n + t;
+}
+
+function f(n, t) {
+  return n + t;
+}
+
+function f(n, t) {
+  return n + t;
+}
+
 function f(a, b) {
   return a + b;
 }
